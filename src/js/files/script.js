196691ee11a715ffea8bd0894955034e30fcef5b
Dima <uvarov3123@gmail.com>
2023-02-14 20:31:58 +0200

    first commit

---
+++ src/js/files/script.js
@@ -0,0 +1,82 @@
+// Подключение функционала "Чертогов Фрилансера"
+import { addTouchClass, isMobile } from "./functions.js";
+// Подключение списка активных модулей
+import { flsModules } from "./modules.js";
+
+addTouchClass()
+
+document.documentElement.addEventListener('click', documentActions)
+
+function documentActions(event) {
+
+    const targerElement = event.target;
+    const html = document.documentElement;
+
+
+    if (targerElement.closest('.catalog-btn')) {
+        if (!html.closest('.catalog-open')) {
+            // если каталог не открыт
+            if (document.querySelector('.content-catalog__item.active')) {
+                document.querySelector('.content-catalog__item.active').classList.remove('active')
+            }
+
+            const catalogItem = document.querySelectorAll('.content-catalog__item')
+            catalogItem[0].classList.add('active')
+
+        }
+        else if (document.querySelector('.content-catalog__item.active')) {
+            document.querySelectorAll('.content-catalog__item.active').forEach(element => {
+                element.classList.remove('active')
+            })
+        }
+        html.classList.toggle('catalog-open')
+        html.classList.toggle('lock')
+    }
+
+    if (targerElement.closest('.content-catalog__link') && document.documentElement.closest('.touch')) {
+        event.preventDefault()
+    }
+
+
+    if (targerElement.closest('.categoty-header__link') && window.innerWidth < 992) {
+        event.preventDefault()
+
+        if (!targerElement.closest('.categoty-header__item.active')) {
+            if (document.querySelector('.categoty-header__item.active')) {
+                document.querySelector('.categoty-header__item.active').classList.remove('active')
+            }
+            const parent = targerElement.closest('.categoty-header__item');
+            parent.classList.add('active')
+        }
+
+    }
+
+    if (targerElement.closest('.icon-menu') && !document.documentElement.closest('.menu-open')) {
+        if (document.querySelector('.categoty-header__item.active')) {
+            document.querySelector('.categoty-header__item.active').classList.remove('active')
+        }
+        document.querySelector('.categoty-header__item').classList.add('active')
+    }
+
+
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    const catalogItems = document.querySelectorAll('.content-catalog__item')
+
+    catalogItems.forEach(catalogItem => {
+        catalogItem.addEventListener('mouseenter', (e) => {
+            if (!e.target.closest('.content-catalog__item.active')) {
+                if (document.querySelector('.content-catalog__item.active')) {
+                    document.querySelector('.content-catalog__item.active').classList.remove('active')
+                }
+                e.target.classList.add('active')
+            }
+        })
+    });
+
+
+});
+
+
